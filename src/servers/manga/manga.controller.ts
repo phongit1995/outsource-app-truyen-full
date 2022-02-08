0be7ce2bc9f8c4e598207a54074d24be65eb08a6
Phong <phongit1995@gmail.com>
2022-02-08 17:18:51 +0700

    add github action

--- src/servers/manga/manga.controller.ts
+++ src/servers/manga/manga.controller.ts
@@ -5,7 +5,7 @@ import { CACHE_MANGA_TIME, PAGE_SIZE_CHAPTER } from './../../common/app.constanc
 import { cacheService } from './../../common/cache.helper';
 export const detailMangaController = async (req: Request, res: Response) => {
     const page: number = req.query.page ? parseInt(req.query.page as string) : 1;
-    const keyCache = `cacheMangaDetail-${page}-req.params.slug`;
+    const keyCache = `cacheMangaDetail-${page}-${req.params.slug}`;
     let cacheData = cacheService.get(keyCache);
     if (cacheData) {
         return res.render('manga/detail', cacheData as object);
@@ -14,12 +14,10 @@ export const detailMangaController = async (req: Request, res: Response) => {
     if (!manga) {
         return res.redirect('/');
     }
-    const chapters = await ChapterService.getListChapterMangaByPage(
-        manga._id.toString(),
-        page,
-        PAGE_SIZE_CHAPTER,
-    );
-    const totalManga = await ChapterService.countChapterOfManga(manga._id.toString());
+    const [chapters, totalManga] = await Promise.all([
+        ChapterService.getListChapterMangaByPage(manga._id.toString(), page, PAGE_SIZE_CHAPTER),
+        ChapterService.countChapterOfManga(manga._id.toString()),
+    ]);
     const dataRender = {
         manga: manga,
         chapters: chapters,
