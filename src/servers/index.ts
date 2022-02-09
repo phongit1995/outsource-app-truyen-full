65b430761f1311b9e56458a5b7868522305355d1
Phong <phongit1995@gmail.com>
2022-02-09 15:52:20 +0700

    add slug name

--- src/servers/index.ts
+++ src/servers/index.ts
@@ -3,5 +3,5 @@ import IndexService from './home/home.router';
 import MangaService from './manga/manga.router';
 const router = express.Router();
 router.use('/', IndexService);
-router.use('/truyen', MangaService);
+router.use('/', MangaService);
 export default router;
