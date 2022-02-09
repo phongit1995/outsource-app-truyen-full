8d5275191d52ebb5f9eea32ffda39bce55cecb62
Phong <phongit1995@gmail.com>
2022-02-09 13:05:47 +0700

    add env

--- crawler/index.ts
+++ crawler/index.ts
@@ -4,11 +4,11 @@ import { getMangaInPageLink, getDetailComic, listCommitNotUpdate } from './getMa
 import Redis from 'ioredis';
 import kue from 'kue';
 const redis = new Redis();
-// redis.flushdb((error) => {
-//     if (error) {
-//         console.log('clear cache redis fail');
-//     } else console.log('clear cache redis success');
-// });
+redis.flushdb((error) => {
+    if (error) {
+        console.log('clear cache redis fail');
+    } else console.log('clear cache redis success');
+});
 const queue = kue.createQueue({
     redis: {
         createClientFactory: function () {
@@ -24,8 +24,9 @@ mongoose
     .catch((error: any) => {
         console.log('connect mongodb fail : ', error);
     });
-
-// for (let i = 1; i <= 996; i++) {
+// const totalPage = 996;
+const totalPage = 50;
+// for (let i = 1; i <= totalPage; i++) {
 //     let job = queue
 //         .create('getLinkComic', i)
 //         .attempts(3)
@@ -48,17 +49,17 @@ mongoose
 //     'https://truyenfull.vn/xuyen-qua-chi-ba-ai-phao-hoi/',
 //     '620326f7c645831e707d41b5',
 // );
-// listCommitNotUpdate().then((data) => {
-//     data.forEach((item) => {
-//         let job = queue
-//             .create('getChapterComic', { url: item.url, id: item._id })
-//             .delay(500)
-//             .save(function (error) {
-//                 if (!error) console.log(job.id);
-//                 else console.log(error);
-//             });
-//     });
-// });
+listCommitNotUpdate().then((data) => {
+    data.forEach((item) => {
+        let job = queue
+            .create('getChapterComic', { url: item.url, id: item._id })
+            .delay(500)
+            .save(function (error) {
+                if (!error) console.log(job.id);
+                else console.log(error);
+            });
+    });
+});
 queue.process('getChapterComic', 1, function (job, done) {
     getDetailComic(job.data.url, job.data.id)
         .then((data) => {
