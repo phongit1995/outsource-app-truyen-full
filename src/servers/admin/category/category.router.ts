08a2d91f8dda0c34f4f7d5f0659c2afa33595c67
vuktmt15 <vuktmt15@gmail.com>
2022-02-26 11:57:33 +0700

    update category admin

--- src/servers/admin/category/category.router.ts
+++ src/servers/admin/category/category.router.ts
@@ -1,13 +1,22 @@
 import {Router} from "express";
-import {changeStatus, handleCreate, handleDelete, renderCategory, renderCreate} from './category.controller';
+import {
+    changeStatus,
+    handleCreate,
+    handleDelete,
+    renderCategory,
+    renderCreate, renderUpdate,
+    updateCategory
+} from './category.controller';
 import {Authentication} from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get('/category', Authentication, renderCategory);
 router.get('/category/create', Authentication, renderCreate);
+router.get('/category/update/:id', Authentication, renderUpdate);
 router.post('/category/store', Authentication, handleCreate);
 router.post('/category/delete', Authentication, handleDelete);
 router.post('/category/change-status', Authentication, changeStatus);
+router.post('/category/update', Authentication, updateCategory);
 
 export default router;
--- src/servers/admin/category/category.service.ts
+++ src/servers/admin/category/category.service.ts
@@ -1,8 +1,12 @@
 import {CategoryModel} from '../../../models/category.model';
+import {makeSlug} from "../../../common/text.helper";
 
 export class CategoryService {
     public static getAllCategory() {
         return CategoryModel.find().sort({createdAt: -1});
+    }
+    public static getCategoryById(id) {
+        return CategoryModel.findOne({_id: id});
     }
     public static createCategory({name, title, slug}) {
         const newCategory = new CategoryModel({
@@ -19,6 +23,14 @@ export class CategoryService {
             category.status = 1;
         }
 
+        return category.save();
+    }
+    public static async updateCategory({id, newName, newTitle}) {
+        const category = await CategoryModel.findOne({_id: id});
+
+        category.name = newName;
+        category.title = newTitle;
+        category.slug = makeSlug(newName);
         return category.save();
     }
     public static deleteCategory(id) {
