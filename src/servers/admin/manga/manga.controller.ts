bdfc7bb371e4a77e5029bd6f3d9e47bbabf4bb54
vuktmt15 <vuktmt15@gmail.com>
2022-02-28 11:40:51 +0700

    pagination

--- src/servers/admin/manga/manga.controller.ts
+++ src/servers/admin/manga/manga.controller.ts
@@ -3,9 +3,16 @@ import MangaService from "./manga.service";
 import moment from 'moment';
 
 export const renderManga = async (req: Request|any, res: Response) => {
+    const perPage = 10;   // Amount manga one page
+    const page = req.params.page || 1;
+    const listManga =  await MangaService.getMangaByPage(page, perPage);
+    const countAllManga = await MangaService.getCountAllManga();
+
     const dataRender: object = {
         nameAdmin: req.session.admin.name,
-        listManga: await MangaService.getAllManga(),
+        listManga,
+        current: page,
+        pages: Math.ceil(countAllManga / perPage),
         moment
     };
 
--- src/servers/admin/manga/manga.service.ts
+++ src/servers/admin/manga/manga.service.ts
@@ -1,8 +1,11 @@
 import {MangaModel} from "../../../models/manga.model";
 
 export class MangaService {
-    public static getAllManga() {
-        return MangaModel.find();
+    public static getCountAllManga() {
+        return MangaModel.find().count();
+    }
+    public static getMangaByPage(page, perPage) {
+        return MangaModel.find().skip((perPage * page) - perPage).limit(perPage).sort({createdAt: -1});
     }
     public static deleteManga(id) {
         return MangaModel.deleteOne({_id: id});
