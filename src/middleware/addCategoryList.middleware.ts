0deeba972adf32e8bee12c5feabdf85bdced6696
PhongSotatek <phong.nguyen@sotatek.com>
2022-03-09 13:47:33 +0700

    add ads

--- src/middleware/addCategoryList.middleware.ts
+++ src/middleware/addCategoryList.middleware.ts
@@ -1,17 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 import { ListService } from './../servers/list/list.service';
 import CategoryService from './../servers/category/category.service';
-
+import { SettingService } from './../servers/admin/setting/setting.service';
 export const addCategoryListMiddleware = async (
     req: Request,
     res: Response,
     next: NextFunction,
 ) => {
-    const [categoryList, listCache] = await Promise.all([
+    const [categoryList, listCache, adsSetting] = await Promise.all([
         CategoryService.getListCategoryCache(),
         ListService.getCategoryCache(),
+        SettingService.getSettingAds(),
     ]);
     res.locals.headerCategoryList = categoryList;
     res.locals.headerList = listCache;
+    res.locals.adsSetting = adsSetting;
     next();
 };
--- src/servers/admin/setting/setting.service.ts
+++ src/servers/admin/setting/setting.service.ts
@@ -1,5 +1,6 @@
 import { SettingModel, ESettingEnumType } from './../../../models/setting.model';
-
+const keyCacheSettingAds = 'CACHE_SETTING_ADS';
+import { cacheService } from './../../../common/cache.helper';
 export class SettingService {
     public static async getSettingScripts() {
         const settingScripts = await SettingModel.findOne({
@@ -37,6 +38,19 @@ export class SettingService {
         return SettingModel.create({ type: ESettingEnumType.ads });
     }
     public static async updateSettingAds(data: any) {
+        cacheService.del(keyCacheSettingAds);
         return SettingModel.updateOne({ type: ESettingEnumType.ads }, { ...data });
+    }
+    public static async getSettingAdsCache() {
+        const dataCache = cacheService.get(keyCacheSettingAds);
+        if (dataCache) {
+            return dataCache;
+        }
+        let setting = await SettingModel.findOne({ type: ESettingEnumType.ads });
+        if (setting) {
+            return setting;
+        }
+        setting = await SettingModel.create({ type: ESettingEnumType.ads });
+        cacheService.set(keyCacheSettingAds, 60 * 30);
     }
 }
