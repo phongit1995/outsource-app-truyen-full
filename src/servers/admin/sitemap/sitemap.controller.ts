7f934f229eda00aa8180dc8432a9fdf6434381cc
PhongSotatek <phong.nguyen@sotatek.com>
2022-03-16 16:34:40 +0700

    add site map

--- src/servers/admin/sitemap/sitemap.controller.ts
+++ src/servers/admin/sitemap/sitemap.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { createReadStream } from 'fs';
+import { createReadStream, unlinkSync, existsSync } from 'fs';
 import { SiteMapService } from './sitemap.service';
 const {
     SitemapStream,
@@ -11,6 +11,9 @@ const { resolve, join } = require('path');
 const { createGzip } = require('zlib');
 const { createWriteStream } = require('fs');
 const numberSitemapInPage = 10000;
+export const sitemapIndexController = (req: Request, res: Response) => {
+    res.render('admin/sitemap');
+};
 export const genderNewSiteMapController = async (req: Request, res: Response) => {
     const stream = new SitemapStream({ hostname: 'https://xemtruyen.vn' });
     const [listAuthor, list, categoryList] = await Promise.all([
@@ -119,4 +122,44 @@ const writeXmlFileSiteMapList = (name: string, data: any) => {
         join(__dirname, '../../..', 'public', 'sitemap', name + '.xml'),
     );
     Readable.from(links).pipe(stream).pipe(writeSteam);
+};
+export const genderSiteMapListController = async (req: Request, res: Response) => {
+    const list = await SiteMapService.getList();
+    if (list.length == 0) {
+        return res.send('Lỗi');
+    }
+    const filePath = join(__dirname, '../../..', 'public', 'sitemap', 'danh-sach.xml');
+    if (existsSync(filePath)) {
+        unlinkSync(filePath);
+    }
+    const links = [];
+    list.forEach((item) => {
+        links.push({ url: '/danh-muc/' + item.slug, changefreq: 'daily', priority: 1 });
+    });
+    const stream = new SitemapStream({ hostname: 'https://xemtruyen.vn' });
+    const writeSteam = createWriteStream(filePath);
+    Readable.from(links).pipe(stream).pipe(writeSteam);
+    writeSteam.on('finish', () => {
+        res.send('hello');
+    });
+};
+export const genderSiteMapAuthorController = async (req: Request, res: Response) => {
+    const list = await SiteMapService.getListAllAuthor();
+    if (list.length == 0) {
+        return res.send('Lỗi');
+    }
+    const filePath = join(__dirname, '../../..', 'public', 'sitemap', 'tac-gia.xml');
+    if (existsSync(filePath)) {
+        unlinkSync(filePath);
+    }
+    const links = [];
+    list.forEach((item) => {
+        links.push({ url: '/tac-gia/' + item.slug, changefreq: 'daily', priority: 1 });
+    });
+    const stream = new SitemapStream({ hostname: 'https://xemtruyen.vn' });
+    const writeSteam = createWriteStream(filePath);
+    Readable.from(links).pipe(stream).pipe(writeSteam);
+    writeSteam.on('finish', () => {
+        res.send('hello');
+    });
 };
--- src/servers/admin/sitemap/sitemap.route.ts
+++ src/servers/admin/sitemap/sitemap.route.ts
@@ -1,5 +1,13 @@
 import { Router } from 'express';
-import { genderNewSiteMapController } from './sitemap.controller';
+import {
+    genderNewSiteMapController,
+    genderSiteMapAuthorController,
+    genderSiteMapListController,
+    sitemapIndexController,
+} from './sitemap.controller';
 const router = Router();
+router.get('/sitemap', sitemapIndexController);
 router.get('/sitemap/render', genderNewSiteMapController);
+router.post('/sitemap/list', genderSiteMapListController);
+router.post('/sitemap/author', genderSiteMapAuthorController);
 export default router;
