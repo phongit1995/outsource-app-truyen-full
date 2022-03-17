d275c3b6df3001f0cfd16168ca5b3c92e702ec25
PhongSotatek <phong.nguyen@sotatek.com>
2022-03-17 09:56:20 +0700

    site map update

--- src/servers/sitemap/sitemap.controller.ts
+++ src/servers/sitemap/sitemap.controller.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from 'express';
-import { createReadStream } from 'fs';
+import { createReadStream, existsSync } from 'fs';
 import { join } from 'path';
 export const renderSiteMap = (req: Request, res: Response) => {
-    const readStream = createReadStream(join(__dirname, './../../public/sitemap/sitemap.xml'));
+    console.log('hello');
+    const pathSiteMap = join(__dirname, './../../public/sitemap/sitemap.xml');
+    if (!existsSync(pathSiteMap)) {
+        return res.send('Chưa Có sitemap Vui lòng Tạo');
+    }
+    const readStream = createReadStream(pathSiteMap);
     res.header('Content-Type', 'text/xml; charset=utf-8');
     readStream.pipe(res);
+};
+export const getSiteMapController = (req: Request, res: Response) => {
+    console.log(req.params.name);
+    res.send('hello');
 };
--- src/servers/sitemap/sitemap.route.ts
+++ src/servers/sitemap/sitemap.route.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
-import { renderSiteMap } from './sitemap.controller';
+import { getSiteMapController, renderSiteMap } from './sitemap.controller';
 const route = Router();
+route.get('/sitemap/:name', getSiteMapController);
 route.get('/sitemap.xml', renderSiteMap);
+
 export default route;
